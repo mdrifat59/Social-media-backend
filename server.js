91d2aaf92a2e9f8aba0cc5402ed52a75e56eec01
mdrifat59 <mdrifatulislam59@gmail.com>
2024-06-09 11:04:54 +0600

    mongodb or mongoose install and database connection

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const dotenv = require('dotenv')
 dotenv.config()
+const { connect }=require('./database/dbConfig')
 const express =require("express")
 const cors =require("cors")
 const router = require("./routes")
 
+
+// database connect function
+    connect()
+
+// express and middleware
 const app=express()
 app.use(cors())
 app.use(router)
 
-app.get('/', (req, res)=>{
-    res.send("this is home page")
-})
+ 
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=>{
